ab00f8bde1acf17366123513fbb08c33bd400adf
Kevin Thornhill <kthorn821@gmail.com>
2016-04-28 14:53:12 +0000

    Added logging

--- thingSpeakTest.py
+++ thingSpeakTest.py
@@ -1,13 +1,19 @@
 import requests
+import logging
+
 
 url = 'https://api.thingspeak.com/update'
 
+FORMAT = '%(asctime)-15s %(message)s'
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG, format=FORMAT)
+
 keywordTest = {'key': 'VZ7NX36F9R4CK5GZ'}
 
-field1 = 55
-field2 = 70
+field1 = 40
+field2 = 45
 
 r = requests.post(url, data={'key': 'VZ7NX36F9R4CK5GZ', 'field1': field1, 'field2': field2},)
-print("URL: ", r.url, " Encoding:", r.encoding, "Field1: ", field1, "Field2: ", field2)
-print(r.text)
+logging.debug('Test Channel Upload- Field1: %s, Field2: %s', field1, field2)
+
 
